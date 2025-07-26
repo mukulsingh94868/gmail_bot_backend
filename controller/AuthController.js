b82b348d9ee7b37b33fdc3f121b14064dd4c8ef3
mukulsingh94868 <mukulsingh94868@gmail.com>
2025-07-26 16:56:00 +0530

    auth serveice

---
+++ controller/AuthController.js
@@ -0,0 +1,79 @@
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import Auth from "../Model/AuthModel.js";
+
+dotenv.config();
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
+// Register a new user
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await Auth.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ message: "Email already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = new Auth({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+
+    res.status(201).json({
+      statusCode: 201,
+      message: "User registered successfully",
+      userId: user?._id,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
+
+    const user = await Auth.findOne({ email }).select("+password");
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id, email: user.email },
+      SECRET_KEY,
+      { expiresIn: "7d" }
+    );
+
+    res.status(200).json({
+      statusCode: 200,
+      message: "Login successful",
+      token,
+      data: {
+        userId: user._id,
+        email: user.email,
+        name: user.name,
+      },
+    });
+  } catch (error) {
+    console.error("Login Error:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- server.js
+++ server.js
@@ -2,9 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import mongoose from 'mongoose';
-// const authRoutes = require('./routes/authRoutes');
 
-// import AuthRoutes from './routes/userRoutes.js';
+import AuthRoutes from './Routes/AuthRoutes.js';
 
 dotenv.config();
 
@@ -12,15 +11,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-// Middleware
 app.use(cors());
-// app.use(express.json());
 
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
 app.use("/api/uploads", express.static("uploads"));
-// app.use('/api/auth', AuthRoutes);
+app.use('/api/auth', AuthRoutes);
 
 mongoose.connect(MONGO_URI).then(() => {
         console.log('DB connection established');
