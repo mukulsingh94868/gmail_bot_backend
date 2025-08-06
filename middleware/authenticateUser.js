b1af09ac830148121f8bd7c9a7aebfe76dacdca3
mukulsingh94868 <mukulsingh94868@gmail.com>
2025-08-06 16:49:31 +0530

    save

--- middleware/authenticateUser.js
+++ middleware/authenticateUser.js
@@ -5,9 +5,9 @@ const authenticateUser = async (req, res, next) => {
   const token = req.header("Authorization")?.replace("Bearer ", "");
   if (!token)
     return res
-      .status(401)
-      .json({ message: "Access Denied: No token provided" });
-
+  .status(401)
+  .json({ message: "Access Denied: No token provided" });
+  
   try {
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
     const user = await Auth.findById(decoded?.userId);
